d8ab36b9748afbe31c4c4b832afbacef760a0150
bernardo <bernardoseven@gmail.com>
2015-11-27 20:01:15 -0300

    mejoras controlador registro cedentes

--- src/java/Controlador/Cliente.java
+++ src/java/Controlador/Cliente.java
@@ -28,7 +28,7 @@ public class Cliente extends HttpServlet {
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException if an I/O error occurs.
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
